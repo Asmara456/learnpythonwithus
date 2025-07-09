38d1cdb136057cb41a79aa81c73178f533aa90bb
Asmara456 <asmaramehboob@gmail.com>
2025-07-09 20:08:05 +0500

    Add files via upload

---
+++ Untitled-2.py
@@ -0,0 +1,33 @@
+# title 
+print ("======== introduction form ======\n")
+
+# asking user name 
+name= input("full_name:")
+age=input("Age:")
+gender=input(" Gender( male/female):")
+nationality=input("country name:")
+email=input("Email;")
+cell=input("phone number:")
+cnic=input("CNIC:")
+dob=input("DOB:")
+religion=input("Religion:")
+qualification=input("Qualification:")
+marital_status=input("status(married/unmarried)")
+#displaying final output 
+print("===== introduction form======\n")
+print(f"Name       : {name}")
+print(f"AGE      : {age}")
+print(f"Gender      : {gender}")
+print(f"Nationality     : {nationality}")
+print(f"Email       : {email}")
+print(f"CELL       : {cell}")
+print(f"CNIC      : {cnic}")
+print(f"DOB       : {dob}")
+print(f"Religion       : {religion}")
+print(f"Qualification      : {qualification}")
+print(f"marital      : {marital_status}")
+
+
+
+print("\n thankyou for filling have a nice day !")
+
